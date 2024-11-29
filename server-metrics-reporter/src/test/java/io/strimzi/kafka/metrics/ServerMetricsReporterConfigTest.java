604cacfb3f9205e9094d0d47070e933f017b2fd3
Mickael Maison <mickael.maison@gmail.com>
2024-11-29 14:48:51 +0100

    Add support for dynamic configurations
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

---
+++ server-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/ServerMetricsReporterConfigTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.metrics;
+
+import io.prometheus.metrics.model.registry.PrometheusRegistry;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.strimzi.kafka.metrics.ClientMetricsReporterConfig.ALLOWLIST_CONFIG;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ServerMetricsReporterConfigTest {
+
+    @Test
+    public void testReconfigure() {
+        Map<String, String> props = new HashMap<>();
+        props.put(ALLOWLIST_CONFIG, "pattern1");
+        ServerMetricsReporterConfig config = new ServerMetricsReporterConfig(props, new PrometheusRegistry());
+        assertTrue(config.allowlist().pattern().contains("pattern1"));
+
+        props.put(ALLOWLIST_CONFIG, "pattern2");
+        config.reconfigure(props);
+        assertFalse(config.allowlist().pattern().contains("pattern1"));
+        assertTrue(config.allowlist().pattern().contains("pattern2"));
+    }
+}
+
